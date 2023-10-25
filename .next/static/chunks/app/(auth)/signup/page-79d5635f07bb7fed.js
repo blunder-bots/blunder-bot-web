ea7d4c0ad6643cd7bd3e6045f91d992f9e1f67b3
umarfarooq478 <muhammadumarfarooq478@gmail.com>
2023-10-25 19:43:51 +0500

    Done

---
+++ .next/static/chunks/app/(auth)/signup/page-79d5635f07bb7fed.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[129,161,277],{6834:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[724,971,864,744],function(){return n(n.s=6834)}),_N_E=n.O()}]);
---
+++ components/ui/header.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import MobileMenu from './mobile-menu'
+import Logo from './Logo/Logo.png'
+import { url } from 'inspector'
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/" className="block" aria-label="Cruip">
+                  Blunder Bots Technologies
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                  Contact Us Now
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+
+        </div>
+      </div>
+    </header>
+  )
+}
